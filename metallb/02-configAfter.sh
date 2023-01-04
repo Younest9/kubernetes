4955a939a64159a9c5269a1aa4cb67354cdcf1ed
unknown <117833467+Younest9@users.noreply.github.com>
2023-01-04 17:02:45 +0100

    install metallb & traefik & config them & things left to verfiy

--- metallb/02-configAfter.sh
+++ metallb/02-configAfter.sh
@@ -1,14 +1,1 @@
-#Make sure it's not reserved (<range-of-ip-addresses>)
-
-cat <<EOF | tee /root/metallb-pool.yaml
-apiVersion: metallb.io/v1beta1
-kind: IPAddressPool
-metadata:
-  name: first-pool
-  namespace: metallb-system
-spec:
-  addresses:
-  - <range-of-ip-addresses>
-EOF
-
-kubectl apply -f /root/metallb-pool.yaml
+kubectl apply -f metallb-pool.yaml
