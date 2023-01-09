68a8202b8ada10188522afad153e510c550d4f86
unknown <117833467+Younest9@users.noreply.github.com>
2023-01-09 15:43:02 +0100

    Join the cluster from the worker nodes

--- debian/k8s cluster/03-join.sh
+++ debian/k8s cluster/03-join.sh
@@ -2,6 +2,5 @@
 # On worker nodes only
 #########################################
 
- 
 kubeadm join <ip-address-of-cluster>:6443 --token <token-generated> \
         --discovery-token-ca-cert-hash sha256:<sha256-hash-generated> #--cri-socket unix:///var/run/cri-dockerd.sock  ##If necessary
