688bd699861fee84ee05ad13c315b898520c85f5
unknown <y.elarjouni.sq@gmail.com>
2023-01-09 16:56:35 +0100

    config & install rancher on a docker container

--- rancheros/rancher/01-install.sh
+++ rancheros/rancher/01-install.sh
@@ -1,2 +1,8 @@
+#disable tls on docker because i don't have ssl certs
 sudo ros config set rancher.docker.tls false
-docker run -d --restart=unless-stopped -p 9080:80 -p 9443:443 --privileged --name rancher-server rancher/rancher:latest
+
+#restart docker
+sudo system-docker restart docker
+
+#Run a docker container with rancher in it
+docker run -d --restart=unless-stopped -p <HOST_PORT_HTTP>:80 -p <HOST_PORT_HTTPS>:443 --privileged --name <CONTAINER_NAME> rancher/rancher:latest
