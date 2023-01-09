fd9547451cf4152fad34458034f383060e273fe1
unknown <y.elarjouni.sq@gmail.com>
2023-01-09 14:55:08 +0100

    MAJ files

---
+++ debian/metallb/02-config.sh
@@ -0,0 +1,17 @@
+#In order to assign an IP to the services, MetalLB must be instructed to do so via the IPAddressPool CR.
+#All the IPs allocated via IPAddressPools contribute to the pool of IPs that MetalLB uses to assign IPs to services.
+#Note: metallb hand out ip addresses to services type "LoadBalancer" only 
+cat <<EOF | tee /root/metallb-pool.yaml
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: first-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  #Make sure it's not reserved
+  - <range-of-ip-addresses>
+EOF
+
+#Apply the manifest to specify the ip address pool of metallb 
+kubectl apply -f metallb-pool.yaml
