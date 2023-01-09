46a8fc32c07d0b6e9bcb10909ee4891e737631fd
unknown <y.elarjouni.sq@gmail.com>
2023-01-09 14:52:36 +0100

    Install & config k3s server

--- debian/k3s+rancher/rancher/01-install.sh
+++ debian/k3s+rancher/rancher/01-install.sh
@@ -1,3 +1,42 @@
+###Prequisities
+###config rancher on k3s cluster
+
+##add Helm chart repository that contains charts to install Rancher
+#Latest: Recommended for trying out the newest features
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+#Stable: Recommended for production environments
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+#Alpha: Experimental preview of upcoming releases.
+helm repo add rancher-alpha https://releases.rancher.com/server-charts/alpha
+
+###Create a Namespace for Rancher
+kubectl create namespace cattle-system
+
+###Choose your SSL Configuration
+#Rancher Generated Certificates (Default)	: ingress.tls.source=rancher : Requires cert-manager : yes
+#Let’s Encrypt : ingress.tls.source=letsEncrypt : Requires cert-manager : yes
+#Certificates from Files : ingress.tls.source=secret : Requires cert-manager : no
+##Install cert-manager (Required case only)
+
+#This step is only required to use certificates issued by Rancher's generated CA (ingress.tls.source=rancher) or to request Let's Encrypt issued certificates (ingress.tls.source=letsEncrypt).
+# If you have installed the CRDs manually instead of with the `--set installCRDs=true` option added to your Helm install command, you should upgrade your CRD resources before upgrading the Helm chart:
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
+
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
+helm install cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.7.1
+#Verify if it is deployed correctly by checking the cert-manager namespace for running pods
+kubectl get pods --namespace cert-manager
+
+
 ###Install Rancher with Helm and Your Chosen Certificate Option
 #This final command to install Rancher requires a domain name that forwards traffic to Rancher. If you are using the Helm CLI to set up a proof-of-concept, you can use a fake domain name when passing the hostname option. An example of a fake domain name would be <IP_OF_LINUX_NODE>.sslip.io, which would expose Rancher on an IP where it is running. Production installs would require a real domain name.
 
@@ -5,7 +44,7 @@
 helm install rancher rancher-<CHART_REPO>/rancher \
   --namespace cattle-system \
   --set replicas=<NUMBER_OF_REPLICAS>
-  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \
+  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \ #Or specify the domaine name if you have it
   --set bootstrapPassword=<PASSWORD_OF_UI_DASHBOARD>
 #If you are installing an alpha version, Helm requires adding the --devel option to the install command:
 helm install rancher rancher-alpha/rancher --devel
@@ -18,7 +57,7 @@ helm install rancher rancher-alpha/rancher --devel
 #Set letsEncrypt.ingress.class to whatever your ingress controller is, e.g., traefik, nginx, haproxy, etc.
 helm install rancher rancher-<CHART_REPO>/rancher \
   --namespace cattle-system \
-  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \
+  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \ #Or specify the domaine name if you have it
   --set replicas=<NUMBER_OF_REPLICAS> \
   --set bootstrapPassword=<PASSWORD_OF_UI_DASHBOARD> \
   --set ingress.tls.source=letsEncrypt \
@@ -32,7 +71,7 @@ helm install rancher rancher-<CHART_REPO>/rancher \
 #Set ingress.tls.source to secret.
 helm install rancher rancher-<CHART_REPO>/rancher \
   --namespace cattle-system \
-  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \
+  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \ #Or specify the domaine name if you have it
   --set replicas=<NUMBER_OF_REPLICAS> \
   --set bootstrapPassword=<PASSWORD_OF_UI_DASHBOARD> \
   --set ingress.tls.source=secret
@@ -40,7 +79,7 @@ helm install rancher rancher-<CHART_REPO>/rancher \
 #If you are using a Private CA signed certificate , add --set privateCA=true to the command:
 helm install rancher rancher-<CHART_REPO>/rancher \
   --namespace cattle-system \
-  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \
+  --set hostname=<IP_OF_LINUX_NODE>.sslip.io \ #Or specify the domaine name if you have it
   --set replicas=<NUMBER_OF_REPLICAS> \
   --set bootstrapPassword=<PASSWORD_OF_UI_DASHBOARD> \
   --set privateCA=true
--- debian/k8s+flannel/02-init.sh
+++ debian/k8s+flannel/02-init.sh
@@ -15,30 +15,8 @@ kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/v0.20.2/Do
 
 
 ##Optional
-#Aliases ans bash completion
+#Bash completion
 apt-get install bash-completion
 
 echo "source /usr/share/bash-completion/bash_completion" >> ~/.bashrc
-
 kubectl completion bash | tee /etc/bash_completion.d/kubectl > /dev/null
-echo 'alias k=kubectl' >>~/.bashrc
-echo "alias kg='kubectl get'" >>~/.bashrc
-echo "alias kgnd='kubectl get nodes'" >>~/.bashrc
-echo "alias kgns='kubectl get namespaces'" >>~/.bashrc
-echo "alias kgp='kubectl get pods'" >>~/.bashrc
-echo "alias kgd='kubectl get deploy'" >>~/.bashrc
-echo "alias kgs='kubectl get svc'" >>~/.bashrc
-echo "alias kgi='kubectl get ingress'" >>~/.bashrc
-echo "alias kgas='kubectl get all -n kube-system'" >>~/.bashrc
-echo "alias kga='kubectl get all'" >>~/.bashrc
-echo "alias kgaa='kubectl get all -A'" >>~/.bashrc
-echo "alias kgan='kubectl get all -n'" >>~/.bashrc
-echo "alias kl='kubectl logs'" >>~/.bashrc
-echo "alias klf='kubectl logs -f'" >>~/.bashrc
-echo "alias ke='kubectl edit'" >>~/.bashrc
-echo "alias kd='kubectl delete'" >>~/.bashrc
-echo "alias kcf='kubectl create -f'" >>~/.bashrc
-echo "alias kaf='kubectl apply -f'" >>~/.bashrc
-echo "alias kdf='kubectl delete -f'" >>~/.bashrc
-echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
-exec bash
