a68d1d72555e23fb4e4f5cedec4140d33576ba38
unknown <y.elarjouni.sq@gmail.com>
2023-01-09 14:58:02 +0100

    Setup & config k3s agent

--- debian/k3s+rancher/k3s/03-join.sh
+++ debian/k3s+rancher/k3s/03-join.sh
@@ -1,5 +1,6 @@
 #Join k3s agent (worker node) to the server (k3s cluster) 
 curl -sfL https://get.k3s.io | K3S_TOKEN=<TOKEN> INSTALL_K3S_EXEC="agent" sh -s - --server https://<IP-ADDRESS-OF-SERVER>:6443 -
 
+
 #Labeling k3s agent (worker node)
 kubectl label nodes <HOSTNAME-OF-THE-NODE>  kubernetes.io/role=workernode # --overwrite if necessary
