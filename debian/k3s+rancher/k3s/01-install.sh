1495ee0e007e28e301734fe92230396edec99e6b
unknown <117833467+Younest9@users.noreply.github.com>
2023-01-09 14:52:16 +0100

    Install & config k3s

--- debian/k3s+rancher/k3s/01-install.sh
+++ debian/k3s+rancher/k3s/01-install.sh
@@ -1,2 +1,30 @@
-#K3S v1.24.9+k3s1
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.24.9+k3s1 K3S_KUBECONFIG_MODE="644" sh -
+##Preconfig (prerequisites)
+
+#Forwarding IPv4 and letting iptables see bridged traffic
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+##Deploy k3s server (cluster) without embedded DB etcd and without traefik
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=<RELEASE_VERSION> K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=<TOKEN> sh -s - server --cluster-init --disable traefik
+
+#Install kubectl to interact with the cluster
+apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+
+##install helm to deploy apps to the cluster easily
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
--- debian/k3s+rancher/k3s/02-config.sh
+++ debian/k3s+rancher/k3s/02-config.sh
@@ -2,7 +2,8 @@
 cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
 
 ###change file permissions
-chmod 700 /root/.kube/config
+chmod 644 /root/.kube/config
 
-###install helm
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+###Send the kubeconfig file to your personal workstation to interact with the cluster from there
+##Warning!!! do not forget to backup any kubeconfig file located in ~/.kube/ because this command below will overwrite any existing kubeconfig
+scp /root/.kube/config <USERNAME>@<IP_ADDRESS_OF_WORKSTATION>:~/.kube/config
