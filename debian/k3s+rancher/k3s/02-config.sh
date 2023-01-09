ecee76f7d14d021907050c997f1612cd1cf91660
unknown <y.elarjouni.sq@gmail.com>
2023-01-09 14:56:30 +0100

    Config k3s server

--- debian/k3s+rancher/k3s/02-config.sh
+++ debian/k3s+rancher/k3s/02-config.sh
@@ -4,6 +4,7 @@ cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
 ###change file permissions
 chmod 644 /root/.kube/config
 
+
 ###Send the kubeconfig file to your personal workstation to interact with the cluster from there
 ##Warning!!! do not forget to backup any kubeconfig file located in ~/.kube/ because this command below will overwrite any existing kubeconfig
 scp /root/.kube/config <USERNAME>@<IP_ADDRESS_OF_WORKSTATION>:~/.kube/config
