4cd81b51e646557787a7d7c5bcd9f8231f98553b
unknown <y.elarjouni.sq@gmail.com>
2023-01-09 15:42:04 +0100

    Init cluster on master node & install flannel & bash-completion

--- debian/k8s cluster/02-init.sh
+++ debian/k8s cluster/02-init.sh
@@ -7,6 +7,7 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
+
 ### if you forget to copy the contents after kubeadm init command to print the join command
 #kubeadm token create --print-join-command
 
