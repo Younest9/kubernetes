b689a2583857527d35a32e44271d3f264040b18c
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-01-10 16:58:29 +0100

    Expose traefik dashboard on localhost

--- debian/traefik/01-install.sh
+++ debian/traefik/01-install.sh
@@ -3,3 +3,9 @@ helm repo add traefik https://traefik.github.io/charts
 helm repo update
 
 helm install traefik traefik/traefik --values=traefik-values.yaml
+
+#Access dashboard on localhost
+
+kubectl port-forward -n kube-system “$(kubectl get pods -n kube-system| grep ‘^traefik-‘ | awk ‘{print $1}’)” 9000:9000
+
+#Now you can access it here : http://localhost:9000/dashboard/
