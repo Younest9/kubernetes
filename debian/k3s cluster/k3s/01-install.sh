21e273519d123f21336087034b137068e737c25e
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-01-16 17:02:42 +0100

    Add possibility to disable embedded LB
    Disable Klipper (ServiceLB) embedded in k3s

--- debian/k3s cluster/k3s/01-install.sh
+++ debian/k3s cluster/k3s/01-install.sh
@@ -16,11 +16,11 @@ net.ipv4.ip_forward                 = 1
 EOF
 
 ##Deploy k3s server (cluster) without embedded DB etcd and without traefik
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=<RELEASE_VERSION> K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=<TOKEN> sh -s - server --cluster-init --disable traefik --cluster-cidr=<SUBNET> # --nocacerts ## ignorer les certificats tls
-#curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=z2b8l0n@k3s sh -s - server --cluster-init --disable traefik  --cluster-cidr=10.244.0.0/16
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=<RELEASE_VERSION> K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=<TOKEN> sh -s - server --cluster-init --disable servicelb #--disable traefik  --cluster-cidr=<SUBNET> # --nocacerts ## ignorer les certificats tls
+#curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=z2b8l0n@k3s sh -s - server --cluster-init --disable traefik  --cluster-cidr=10.244.0.0/16 --disable servicelb # disable default Loadbalancer
 
 #Install kubectl to interact with the cluster
-apt-get update && sudo apt-get install -y apt-transport-https
+apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
