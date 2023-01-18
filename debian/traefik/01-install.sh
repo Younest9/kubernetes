f27648ccb87161d9f8551e59df05a70c6c3bbd44
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-01-18 10:19:17 +0100

    Add helm chart of traefik

--- debian/traefik/01-install.sh
+++ debian/traefik/01-install.sh
@@ -1,4 +1,4 @@
-helm repo add traefik https://traefik.github.io/charts
+helm repo add traefik https://traefik.github.io/charts # https://helm.traefik.io/traefik  ##helm chart of traefik
 
 helm repo update
 
