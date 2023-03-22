55dfabd222999625de612a7b30b83b1f5b215507
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-03-22 17:14:47 +0100

    Add comments and indications for the commands

--- debian/k3s cluster/k3s/02-config.sh
+++ debian/k3s cluster/k3s/02-config.sh
@@ -1,13 +1,11 @@
 ###Copy config file to .kube directory
-cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
+cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
 
 ###change file permissions
-chmod 644 /root/.kube/config
+chmod 644 $HOME/.kube/config
 
-###Send the kubeconfig file to your personal workstation to interact with the cluster from there
-##Warning!!! do not forget to backup any kubeconfig file located in ~/.kube/ because this command below will overwrite any existing kubeconfig
-scp /root/.kube/config <USERNAME>@<IP_ADDRESS_OF_WORKSTATION>:~/.kube/config
+### Send the kubeconfig file to your personal workstation to interact with the cluster from there
+## Warning!!! do not forget to backup any kubeconfig file located in ~/.kube/ because this command below will overwrite any existing kubeconfig
+scp $HOME/.kube/config <USERNAME>@<IP_ADDRESS_OF_WORKSTATION>:~/.kube/config
 #Or
-scp <USERNAME>@<IP_ADDRESS_OF_WORKSTATION>:~/.kube/config ~/.kube/config
-#scp root@172.20.51.6:/root/.kube/config ~/.kube/config
-#scp root@172.20.51.6:/root/.kube/config .kube/config
+#scp <USERNAME>@<IP_ADDRESS_OF_WORKSTATION>:~/.kube/config ~/.kube/config
