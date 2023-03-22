182cd78e0de8c2fe7e5c8b529a7caa0adc13a4da
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-03-22 17:13:30 +0100

    fix command + add comments

--- debian/k3s cluster/k3s/01-install.sh
+++ debian/k3s cluster/k3s/01-install.sh
@@ -15,9 +15,8 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-##Deploy k3s server (cluster) without embedded DB etcd and without traefik
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=<RELEASE_VERSION> K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=<TOKEN> sh -s - server --cluster-init --disable servicelb #--disable traefik  --cluster-cidr=<SUBNET> # --nocacerts ## ignorer les certificats tls
-#curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=z2b8l0n@k3s sh -s - server --cluster-init --disable traefik  --cluster-cidr=10.244.0.0/16 --disable servicelb # disable default Loadbalancer
+##Deploy k3s server (cluster) / You can disable pretty much everything, if you want to keep something just delete it from the command 
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=<RELEASE_VERSION> K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" K3S_TOKEN=<TOKEN> sh -s - server --cluster-init --disable servicelb --disable traefik  --cluster-cidr=<SUBNET> --nocacerts ## ignorer les certificats tls
 
 #Install kubectl to interact with the cluster
 apt-get update && apt-get install -y apt-transport-https
@@ -26,5 +25,5 @@ echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/s
 apt-get update
 apt-get install -y kubectl
 
-##install helm to deploy apps to the cluster easily
+##install helm to deploy apps to the cluster
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
