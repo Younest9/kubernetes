9551f90f95739e9bdfabb8072116c6175be44a51
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-03-22 17:02:44 +0100

    Disable dashboard port-forwarding

--- debian/traefik/01-install.sh
+++ debian/traefik/01-install.sh
@@ -6,6 +6,6 @@ helm install traefik traefik/traefik --values=traefik-values.yaml
 
 #Access dashboard on localhost
 
-kubectl port-forward -n kube-system “$(kubectl get pods -n kube-system| grep ‘^traefik-‘ | awk ‘{print $1}’)” 9000:9000
+#kubectl port-forward -n kube-system “$(kubectl get pods -n kube-system| grep ‘^traefik-‘ | awk ‘{print $1}’)” 9000:9000
 
 #Now you can access it here : http://localhost:9000/dashboard/
