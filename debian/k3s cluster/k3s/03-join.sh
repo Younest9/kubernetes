b3d8ed3d7b9c195bef171280f290e8b6aed88900
Younest9 <117833467+Younest9@users.noreply.github.com>
2023-03-22 17:15:43 +0100

    Add comments

--- debian/k3s cluster/k3s/03-join.sh
+++ debian/k3s cluster/k3s/03-join.sh
@@ -1,7 +1,6 @@
-#Join k3s agent (worker node) to the server (k3s cluster) 
+# Join k3s agent (worker node) to the server (k3s cluster) 
 curl -sfL https://get.k3s.io | K3S_TOKEN=<TOKEN> INSTALL_K3S_EXEC="agent" sh -s - --server https://<IP-ADDRESS-OF-SERVER>:6443 -
-#curl -sfL https://get.k3s.io | K3S_TOKEN=z2b8l0n@k3s INSTALL_K3S_EXEC="agent" sh -s - --server https://172.20.51.6:6443 -
 
-#Labeling k3s agent (worker node)
-kubectl label nodes <HOSTNAME-OF-THE-NODE>  kubernetes.io/role=worker # --overwrite if necessary
-#kubectl label nodes workernode  kubernetes.io/role=worker
+# Labeling k3s agent (worker node)
+# You can override the cureent label by using the flag --overwrite
+kubectl label nodes <HOSTNAME-OF-THE-NODE>  kubernetes.io/role=worker --overwrite
